96d51c1f3356f8cb1e159a6a20d4b4f856316994
umeshkumar <ukm3423@gmail.com>
2024-07-11 18:18:21 +0530

    please chal ja

--- src/landing/components/WhyUs.jsx
+++ src/landing/components/WhyUs.jsx
@@ -4,7 +4,7 @@ export default function WhyUs() {
   return (
     <div className="bg-gray-100 py-12">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold text-center mb-6">Why Choose LEARNIFY?</h2>
+        <h2 className="text-3xl font-bold text-center mb-6">Why Choose LEARNIFY ?</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 text-center">
           {/* Feature 1 */}
           <div className="p-6 bg-white rounded-lg shadow-md">
